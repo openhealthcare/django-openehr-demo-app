33a42e5a4d3f0b87af449c215cdaae606c1c1ced
Marcus Baw <marcusbaw@gmail.com>
2018-02-05 15:16:47 +0000

    add 0.0.0.0 and localhost to ALLOWED_HOSTS

--- djopenehr/settings.py
+++ djopenehr/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '6rk_6cz(6b_7k15n7nwdj$cat5-80)1(gz(94rfhs-3vcwc%#b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
