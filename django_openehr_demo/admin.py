57977ab00f4fbde183d2ddabb9449d2588800954
Marcus Baw <marcusbaw@gmail.com>
2018-02-05 12:34:08 +0000

    updates to the demo app reflecting further work on the openehr module

--- django_openehr_demo/admin.py
+++ django_openehr_demo/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 import django_openehr.models as ehrmodels
 
 admin.site.register(ehrmodels.AddressDetails)
-admin.site.register(ehrmodels.TelecomDetails)
-admin.site.register(ehrmodels.Demographics)
+admin.site.register(ehrmodels.BodySite)
+admin.site.register(ehrmodels.DemographicPersonal)
+admin.site.register(ehrmodels.DemographicProfessional)
+admin.site.register(ehrmodels.Identifier)
+admin.site.register(ehrmodels.InpatientAdmission)
+admin.site.register(ehrmodels.PersonName)
+admin.site.register(ehrmodels.ReasonForEncounter)
+admin.site.register(ehrmodels.RelevantContact)
 admin.site.register(ehrmodels.SymptomSign)
+admin.site.register(ehrmodels.TelecomDetails)
 admin.site.register(ehrmodels.TherapeuticDirection)
