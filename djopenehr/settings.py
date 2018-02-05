c0cb097e837d61ba96027efd7616c03b4051b543
Marcus Baw <marcusbaw@gmail.com>
2018-02-05 15:05:42 +0000

    heroku specific changes eg switch to postgres

--- djopenehr/settings.py
+++ djopenehr/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(__file__)
@@ -76,12 +81,7 @@ WSGI_APPLICATION = 'djopenehr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Internationalization
