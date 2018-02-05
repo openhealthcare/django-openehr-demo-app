4810ccb0d8e67216f6594aa9ce8ddd9e2e8ad691
Marcus Baw <marcusbaw@gmail.com>
2018-02-05 14:41:50 +0000

    staticfiles fix

--- djopenehr/settings.py
+++ djopenehr/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(__file__)
 
 
 # Quick-start development settings - unsuitable for production
@@ -99,5 +100,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'assets')
 STATIC_URL = '/static/'
