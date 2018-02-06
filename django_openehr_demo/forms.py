4fc44134ad6556111fe55f51cee2f8916c52b83a
Marcus Baw <marcusbaw@gmail.com>
2018-02-06 15:59:53 +0000

    refactored to use django ModelForm idiom in order to reduce repetition

--- django_openehr_demo/forms.py
+++ django_openehr_demo/forms.py
@@ -1,78 +1,77 @@
-from django import forms
-
-def choices(*args):
-    return [(a, a) for a in args]
-
-class RelevantContactForm(forms.Form):
-    REL_CHOICES = choices('Informal Carer', 'Main Informal Carer')
-
-    full_name             = forms.CharField()
-    telecoms              = forms.CharField()
-    professional_group    = forms.CharField()
-    relationship_category = forms.ChoiceField(choices=REL_CHOICES)
-    relationship          = forms.CharField()
-    is_next_of_kin        = forms.BooleanField()
-    note                  = forms.CharField()
-
-
-class AdmissionForm(forms.Form):
-    date_of_admission = forms.DateField()
-    presenting_problem = forms.CharField()
-
-class AllergiesForm(forms.Form):
-    causative_agent = forms.CharField()
-    reaction        = forms.CharField()
-    date_recorded   = forms.DateField()
-
 # IDCR Transfer of Care summary (minimal)
-#
+from django import forms
+import django_openehr.models as ehrmodels
+
+
 # SECTION Relevant contacts [0..*]
 # openEHR-EHR-SECTION.relevant_contacts_rcp.v1
 
-#   ADMIN_ENTRY Relevant contact [0..*]
-#   openEHR-EHR-ADMIN_ENTRY.relevant_contact_rcp.v0
-#
-#      CLUSTER Personal details
-#      openEHR-EHR-CLUSTER.individual_professional_uk.v1
+class DemographicProfessionalForm(forms.ModelForm):
+    # CLUSTER Personal details => openEHR-EHR-CLUSTER.individual_professional_uk.v1
+    #   => INCLUDES CLUSTER Person name => openEHR-EHR-CLUSTER.person_name.v1
+    # INCLUDES CLUSTER Telecom details => openEHR-EHR-CLUSTER.telecom_uk.v1
+    class Meta:
+        model = ehrmodels.DemographicProfessional
+        fields = [
+            'person_name',             # TODO should be ELEMENT Full name FROM PersonName
+            'telecom_details',         # ELEMENT Telecoms
+            'professional_group'       # ELEMENT Professional group
+        ]
 
-#        CLUSTER Person name
-#        openEHR-EHR-CLUSTER.person_name.v1
-#          ELEMENT Full name
+class RelevantContactForm(forms.ModelForm):
+    # ADMIN_ENTRY Relevant contact [0..*]
+    # from openEHR-EHR-ADMIN_ENTRY.relevant_contact_rcp.v0
+    class Meta:
+        model = ehrmodels.RelevantContact
+        fields = [
+            'relationship_category',   # ELEMENT Relationship category
+            'relationship',            # ELEMENT Relationship
+            'is_next_of_kin',          # ELEMENT Is next of kin?
+            'relationship_note',       # ELEMENT Note
+            'date_updated'             # ELEMENT Date updated
+        ]
 
-#        CLUSTER Telecom details
-#        openEHR-EHR-CLUSTER.telecom_uk.v1
-#          ELEMENT Telcoms
-#
-#       ELEMENT Professional group
-
-#   ELEMENT Relationship category
-#   ELEMENT Relationship
-#   ELEMENT Is next of kin?
-#   ELEMENT Note
-#   ELEMENT Date updated
 
 # SECTION Admission details [0..*]
 # openEHR-EHR-SECTION.admission_details_rcp.v1
-#   ADMIN_ENTRY Inpatient admission [0..*]
-#   openEHR-EHR-ADMIN_ENTRY.inpatient_admission_uk.v1
-#     ELEMENT Date of admission
-#
-#   EVALUATION Reason for Encounter [0..*]
-#   openEHR-EHR-EVALUATION.reason_for_encounter.v1
-#     ELEMENT Presenting Problem [0..*]
-#
+
+class AdmissionForm(forms.ModelForm):
+    # ADMIN_ENTRY Inpatient admission [0..*]
+    # openEHR-EHR-ADMIN_ENTRY.inpatient_admission_uk.v1
+    class Meta:
+        model = ehrmodels.InpatientAdmission
+        # ELEMENT Date of admission
+        fields = ['date_of_admission']
+
+class ReasonForEncounterForm(forms.ModelForm):
+    # EVALUATION Reason for Encounter [0..*]
+    # openEHR-EHR-EVALUATION.reason_for_encounter.v1
+    class Meta:
+        model = ehrmodels.ReasonForEncounter
+        # ELEMENT Presenting Problem [0..*]
+        fields = ['presenting_problem']
+
 # SECTION Allergies and adverse reactions [1..1]
 # openEHR-EHR-SECTION.allergies_adverse_reactions_rcp.v1
-#   EVALUATION Adverse reaction [0..*]
-#   openEHR-EHR-EVALUATION.adverse_reaction_uk.v1
-#     ELEMENT Causative agent [1..1] ELEMENT   OR   ELEMENT
-#     ELEMENT Reaction
-#     ELEMENT Date recorded
-#
+
+class AllergiesForm(forms.ModelForm):
+    class Meta:
+        # EVALUATION Adverse reaction [0..*]
+        # openEHR-EHR-EVALUATION.adverse_reaction_uk.v1
+        model = ehrmodels.AdverseReaction
+        fields = [
+            'causative_agent',         # ELEMENT Causative agent [1..1] ELEMENT   OR   ELEMENT
+            'reaction_snomed_code',    # ELEMENT Reaction
+            'date_recorded'            # ELEMENT Date recorded
+        ]
+
+
 # SECTION Medication and medical devices
 # openEHR-EHR-SECTION.medication_medical_devices_rcp.v1
 #   SECTION Current medication
 #   openEHR-EHR-SECTION.current_medication_rcp.v1
+    class MedicationStatementForm():
+        pass
 #   EVALUATION Medication statement [0..*]
 #   openEHR-EHR-EVALUATION.medication_statement_uk.v1
 #
@@ -115,7 +114,8 @@ class AllergiesForm(forms.Form):
 #     CLUSTER Discontinued details
 #     CLUSTER Discontinutation
 #       ELEMENT Reason discontinued [0..*]
-#
+
+
 # SECTION Diagnoses [0..*]
 #   EVALUATION Problem/diagnosis summary [0..*]
 #   openEHR-EHR-EVALUATION.problem_diagnosis.v1
